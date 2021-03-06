bd556e1e9fbe79d5562fdc402d4c2661cd791c65
yasirghouri <yasir.ag7@gmail.com>
2021-03-06 13:19:36 +0500

    added functionality to signIn with email and password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,4 +12,9 @@ export class AuthController {
   ): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
+
+  @Post('/signin')
+  signIn(@Body(ValidationPipe) authCredentionDto: AuthCredentialsDto) {
+    return this.authService.signIn(authCredentionDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -13,5 +13,15 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto) {
+    const username = await this.userRepository.validateUserPassword(
+      authCredentialsDto,
+    );
+
+    if (!username) {
+      throw new UnauthorizedException('Invalid Credentials');
+    }
   }
 }
