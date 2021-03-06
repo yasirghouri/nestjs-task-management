61fe3151dea876b1f7fc88b9a3de9383391ebd37
yasirghouri <yasir.ag7@gmail.com>
2021-03-06 11:08:22 +0500

    added TaskRepository in our app and refactored controller and service code of deleteTask method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -44,10 +44,10 @@ export class TasksController {
   //   return this.tasksService.updateTaskStatus(id, status);
   // }
 
-  // @Delete('/:id')
-  // deleteTask(@Param('id') id: string): void {
-  //   this.tasksService.deleteTask(id);
-  // }
+  @Delete('/:id')
+  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.tasksService.deleteTask(id);
+  }
 
   @Post()
   @UsePipes(ValidationPipe)
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -39,10 +39,14 @@ export class TasksService {
   //   }
   //   return tasks;
   // }
-  // deleteTask(id: string): void {
-  //   const found = this.getTaskById(id);
-  //   this.tasks = this.tasks.filter((task) => task.id !== found.id);
-  // }
+
+  async deleteTask(id: number): Promise<void> {
+    const result = await this.taskRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with id: ${id} not found!`);
+    }
+  }
+
   // updateTaskStatus(id: string, status: TaskStatus): Task {
   //   const task = this.getTaskById(id);
   //   task.status = status;
