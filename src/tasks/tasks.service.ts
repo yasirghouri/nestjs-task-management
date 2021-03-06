5307916bddb355e2367ec8258d456aa646fa5ac6
yasirghouri <yasir.ag7@gmail.com>
2021-03-06 11:41:47 +0500

    added TaskRepository in our app and refactored controller and service code of getTasks method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,9 +13,10 @@ export class TasksService {
     @InjectRepository(TaskRepository)
     private taskRepository: TaskRepository,
   ) {}
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
+
+  getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto);
+  }
 
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
@@ -24,21 +25,6 @@ export class TasksService {
     }
     return found;
   }
-
-  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //   let tasks = this.getAllTasks();
-  //   if (status) {
-  //     tasks = tasks.filter((task) => task.status === status);
-  //   }
-  //   if (search) {
-  //     tasks = tasks.filter(
-  //       (task) =>
-  //         task.title.includes(search) || task.description.includes(search),
-  //     );
-  //   }
-  //   return tasks;
-  // }
 
   async deleteTask(id: number): Promise<void> {
     const result = await this.taskRepository.delete(id);
