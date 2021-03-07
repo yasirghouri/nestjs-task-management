6c348700c7535e6a5d104797b878b305d1e42533
yasirghouri <yasir.ag7@gmail.com>
2021-03-07 14:23:24 +0500

    added test cases for Tasks, testing deleteTask

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -11,6 +11,7 @@ const mockTaskRepository = () => ({
   getTasks: jest.fn(),
   findOne: jest.fn(),
   createTask: jest.fn(),
+  delete: jest.fn(),
 });
 
 describe('TasksService', () => {
@@ -77,6 +78,24 @@ describe('TasksService', () => {
         mockUser,
       );
       expect(result).toEqual('some task');
+    });
+  });
+
+  describe('deleteTask', () => {
+    it('call taskRepository.deleteTask() to delete task', async () => {
+      taskRepository.delete.mockResolvedValue({ affected: 1 });
+      expect(taskRepository.delete).not.toHaveBeenCalled();
+      await tasksService.deleteTask(1, mockUser);
+      expect(taskRepository.delete).toHaveBeenCalledWith({
+        id: 1,
+        userId: mockUser.id,
+      });
+    });
+    it('throws an error as task could not be found', async () => {
+      taskRepository.delete.mockResolvedValue({ affected: 0 });
+      expect(tasksService.deleteTask(1, mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
     });
   });
 });
